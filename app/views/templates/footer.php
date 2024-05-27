bc67b02bd7b47ce8cfe84d39012348e958c106a8
mikhail-karim <mikhailsakarim@gmail.com>
2024-05-27 23:12:29 +0700

    pengadaan footer (masih berantakan)

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,6 +1,52 @@
 <!-- CDN -->
+
+<footer class="footer text-center text-md-left">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 mb-3">
+                <h5 class="font-weight-bold">RentACar.com</h5>
+                <p>Travel easily.</p>
+            </div>
+            <div class="col-md-3 mb-3">
+                <h5 class="font-weight-bold">Kerjasama</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">Mendaftar Mitra</a></li>
+                    <li><a href="#">Kantor Cabang</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 mb-3">
+                <h5 class="font-weight-bold">Layanan Pelanggan</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">Kantor Cabang</a></li>
+                    <li><a href="#">Pengembalian Dana</a></li>
+                    <li><a href="#">Pusat Bantuan</a></li>
+                    <li><a href="#">Laporkan Masalah</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 mb-3">
+                <h5 class="font-weight-bold">Download Aplikasi</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#"><img src="https://img.icons8.com/ios-filled/24/ffffff/google-play.png"/> Google Play</a></li>
+                    <li><a href="#"><img src="https://img.icons8.com/ios-filled/24/ffffff/apple-app-store.png"/> App Store</a></li>
+                </ul>
+                <h5 class="font-weight-bold mt-3">Media sosial kami</h5>
+                <div class="social-icons">
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                    <a href="#"><i class="fab fa-tiktok"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.4.slim.min.js" integrity="sha256-a2yjHM4jnF9f54xUQakjZGaqYs/V1CYvWpoqZzC2/Bw=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 <script src="<?= BASEURL; ?>/js/script.js"></script>
 </body>
 
