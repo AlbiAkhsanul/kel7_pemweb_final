23cb49949524dc0fe32c2a16f1922fa4e2ec624f
Rhaeyvan Phasca Sanggam Sinaga <mmivanchanel@gmail.com>
2024-05-31 01:12:15 +0700

    Delete app/views directory

--- app/views/home/index.php
+++
@@ -1,7 +0,0 @@
-<div class="container mt-3">
-    <div class="jumbotron">
-        <h1 class="display-4">Welcome To My Homepage</h1>
-        <p class="lead">
-        <h3>Hi!</h3>
-    </div>
-</div>
