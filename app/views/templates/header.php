0054ae9305db051cf8243872f2cacd39122bd49a
mikhail-karim <mikhailsakarim@gmail.com>
2024-05-31 06:00:23 +0700

    initial redesigns

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $data['title']; ?></title>
-  <!-- CDN -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Plus Jakarta Sans:wght@400;600;700&display=swap" />
-  <link rel="stylesheet" href="css/styles.css" >
-</head>
-
-<body>
 
   <!-- <nav class="header">
       <div class="logo-lock-up">
@@ -61,28 +47,3 @@
     </div>
   </nav> -->
 
-<header class="p-3 text-bg-dark">
-  <div class="container">
-    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start navbar-fixed-top">
-      <a href="" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <img class="bi me-2" width="213" height="48" alt="Logo" src="src/logobrand.svg">
-      </a>
-      <ul class="nav col-12 col-lg-auto ms-auto me-4 mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 text-white">Mendaftar Mitra</a></li>
-        <li><a href="#" class="nav-link px-2 text-white">Transaksi</a></li>
-        <li><a href="#" class="nav-link px-2 text-white">Bantuan</a></li>
-        <li><a href="#" class="nav-link px-2 text-white">Laporkan Masalah</a></li>
-        <!-- <li><a href="#" class="nav-link px-2 text-white">About</a></li> -->
-      </ul>
-
-      <!-- <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-        <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
-      </form> -->
-
-      <div class="text-end">
-        <button type="button" class="btn btn-outline-light me-2">Masuk | Daftar</button>
-        <!-- <button type="button" class="btn btn-warning">Sign-up</button> -->
-      </div>
-    </div>
-  </div>
-</header>
