bb939151adf543e3b2baaf88e86e889737fa9363
Albi Akhsanul H <albiakhsanulhakim24@gmail.com>
2024-05-31 20:09:46 +0700

    driver controller n model (unfinished)

---
+++ app/controllers/Driver.php
@@ -0,0 +1,140 @@
+<?php
+
+class Driver extends Controller
+{
+    public function index()
+    {
+        if (!isset($_SESSION["login"])) {
+            header("Location: " . BASEURL . "/auth/login");
+            exit;
+        }
+        if ($_SESSION["is_admin"] !== 1) {
+            header("Location: " . BASEURL . "/home");
+            exit;
+        }
+        $data['title'] = 'Car List';
+        $data['cars'] = $this->model('Car_model')->getAllCars();
+        $this->view('templates/header', $data);
+        $this->view('car/index', $data);
+        $this->view('templates/footer');
+    }
+
+    public function create()
+    {
+        if (!isset($_SESSION["login"])) {
+            header("Location: " . BASEURL . "/auth/login");
+            exit;
+        }
+        if ($_SESSION["is_admin"] !== 1) {
+            header("Location: " . BASEURL . "/home");
+            exit;
+        }
+
+        $data['title'] = 'Create Car';
+        $this->view('templates/header', $data);
+        $this->view('car/create', $data);
+        $this->view('templates/footer');
+    }
+
+    public function store()
+    {
+        if (!isset($_SESSION["login"])) {
+            header("Location: " . BASEURL . "/auth/login");
+            exit;
+        }
+        if ($_SESSION["is_admin"] !== 1) {
+            header("Location: " . BASEURL . "/home");
+            exit;
+        }
+        if (empty($_POST) && empty($_FILES)) {
+            header('Location: ' . BASEURL . '/car/create');
+            exit;
+        }
+        if ($this->model('Car_model')->createNewCar($_POST, $_FILES) > 0) {
+            FlashMsg::setFlash('Succesfully', 'Created', 'success');
+            header('Location: ' . BASEURL . '/car');
+            exit;
+        } else {
+            FlashMsg::setFlash('Unsuccesfully', 'Created', 'danger');
+            header('Location: ' . BASEURL . '/car');
+            exit;
+        }
+    }
+
+    public function show($id)
+    {
+        $data['title'] = 'Car Details';
+        $data['car'] = $this->model('Car_model')->getCarById($id);
+        $this->view('templates/header', $data);
+        $this->view('car/details', $data);
+        $this->view('templates/footer');
+    }
+
+    public function edit($id)
+    {
+        if (!isset($_SESSION["login"])) {
+            header("Location: " . BASEURL . "/auth/login");
+            exit;
+        }
+        if ($_SESSION["is_admin"] !== 1) {
+            header("Location: " . BASEURL . "/home");
+            exit;
+        }
+        $data['title'] = 'Edit Car';
+        $data['car'] = $this->model('Car_model')->getCarById($id);
+        $this->view('templates/header', $data);
+        $this->view('car/edit', $data);
+        $this->view('templates/footer');
+    }
+
+    public function update($id)
+    {
+        if (!isset($_SESSION["login"])) {
+            header("Location: " . BASEURL . "/auth/login");
+            exit;
+        }
+        if ($_SESSION["is_admin"] !== 1) {
+            header("Location: " . BASEURL . "/home");
+            exit;
+        }
+        if ($this->model('Car_model')->editCarById($_POST, $_FILES, $id) > 0) {
+            FlashMsg::setFlash('Succesfully', 'Edited', 'success');
+            header('Location: ' . BASEURL . '/car');
+            exit;
+        } else {
+            FlashMsg::setFlash('Unsuccesfully', 'Edited', 'danger');
+            header('Location: ' . BASEURL . '/car');
+            exit;
+        }
+    }
+
+    public function delete($id)
+    {
+        if (!isset($_SESSION["login"])) {
+            header("Location: " . BASEURL . "/auth/login");
+            exit;
+        }
+        if ($_SESSION["is_admin"] !== 1) {
+            header("Location: " . BASEURL . "/home");
+            exit;
+        }
+        if ($this->model('Car_model')->deleteCarById($id) > 0) {
+            FlashMsg::setFlash('Succesfully', 'Deleted', 'success');
+            header('Location: ' . BASEURL . '/car');
+            exit;
+        } else {
+            FlashMsg::setFlash('Unsuccesfully', 'Deleted', 'danger');
+            header('Location: ' . BASEURL . '/car');
+            exit;
+        }
+    }
+
+    public function search()
+    {
+        $data['title'] = 'Car List';
+        $data['cars'] = $this->model('Car_model')->getCarsByKeyword();
+        $this->view('templates/header', $data);
+        $this->view('car/index', $data);
+        $this->view('templates/footer');
+    }
+}
---
+++ app/models/Driver_model.php
@@ -0,0 +1,176 @@
+<?php
+
+class Driver_model
+{
+    private $table_name = 'cars';
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function getAllCars()
+    {
+        $this->db->query("SELECT * FROM {$this->table_name}");
+        return $this->db->resultSet();
+    }
+
+    public function createNewCar($data, $dataImg)
+    {
+        // $currentTime = date('Y-m-d H:i');
+        $currentTime = date('Y-m-d');
+        $carImg = $this->uploadCarImg($dataImg, $currentTime);
+        $query = "INSERT INTO {$this->table_name} (branch_id,car_brand_id,nama_mobil,jenis_mobil,tipe_transmisi,harga_sewa,foto_mobil,status_mobil) VALUES 
+                  (:branch_id,:car_brand_id,:nama_mobil,:jenis_mobil,:tipe_transmisi,:harga_sewa,:foto_mobil,:status_mobil)";
+        $this->db->query($query);
+        $this->db->bind('branch_id', $data['branch_id']);
+        $this->db->bind('car_brand_id', $data['car_brand_id']);
+        $this->db->bind('nama_mobil', $data['nama_mobil']);
+        $this->db->bind('jenis_mobil', $data['jenis_mobil']);
+        $this->db->bind('tipe_transmisi', $data['tipe_transmisi']);
+        $this->db->bind('harga_sewa', $data['harga_sewa']);
+        $this->db->bind('foto_mobil', $carImg);
+        $this->db->bind('status_mobil', $data['status_mobil']);
+
+        $this->db->execute();
+        return $this->db->affectedRowCount();
+    }
+
+    public function uploadCarImg($dataImg, $seconds)
+    {
+        $fileName = $dataImg['foto_mobil']['name'];
+        $fileSize = $dataImg['foto_mobil']['size'];
+        $fileError = $dataImg['foto_mobil']['error'];
+        $fileTmpLocation = $dataImg['foto_mobil']['tmp_name'];
+
+        // Cek apakah gambar ada yang diupload
+        if ($fileError === 4) {
+            echo "
+            <script>
+                alert('Pilih foto Untuk Mobil');
+            </script>
+        ";
+
+            exit;
+        }
+
+        // Cek apakah yang dikirim merupakan gambar
+        $validImagaeExtension = ['jpg', 'jpeg', 'png'];
+        $imageExtension = explode('.', $fileName);
+        // Mengambil bagian ekstensi gambar saja (.jpg dll)
+        $imageExtension = strtolower(end($imageExtension)); //Diubah menjadi huruf kecil
+        if (!in_array($imageExtension, $validImagaeExtension)) {
+            echo "
+            <script>
+                alert('Gunakan Tipe File Yang Valid (jpg,jpeg,png) ');
+            </script>
+        ";
+
+            exit;
+        }
+
+        // Cek ukuran
+        if ($fileSize > 1500000) {
+            echo "
+            <script>
+                alert('Ukuran foto Terlalu Besar');
+            </script>
+        ";
+
+            exit;
+        }
+
+        // Gambar siap diupload
+        // Generate nama baru
+        $fileNameNew = explode('.', $fileName);
+        $fileNameNew = $fileNameNew[0] . $seconds . rand(1, 9) . rand(1, 9) . rand(1, 9) . rand(1, 9) . rand(1, 9) . rand(1, 9) . rand(1, 9) . rand(1, 9) . '.' . end($fileNameNew);
+        move_uploaded_file($fileTmpLocation, 'img/cars/' . $fileNameNew);
+        return $fileNameNew;
+    }
+
+    public function getCarById($id)
+    {
+        $this->db->query("SELECT * FROM {$this->table_name} WHERE car_id=:car_id");
+        // untuk menghindari sql injection
+        $this->db->bind('car_id', $id);
+        return $this->db->single();
+    }
+
+    public function editCarById($data, $dataImg, $id)
+    {
+        if ($dataImg['foto_mobil']['error'] === 4) {
+            $carImg = $data['old_foto_mobil'];
+        } else {
+            $currentTime = date('Y-m-d');
+            $this->unlinkCarImg($id);
+            $carImg = $this->uploadCarImg($dataImg, $currentTime);
+        }
+
+        $query = "UPDATE {$this->table_name} SET 
+                  branch_id = :branch_id, 
+                  car_brand_id = :car_brand_id,
+                  nama_mobil = :nama_mobil, 
+                  jenis_mobil = :jenis_mobil,
+                  tipe_transmisi = :tipe_transmisi,
+                  harga_sewa = :harga_sewa,
+                  foto_mobil = :foto_mobil,
+                  status_mobil = :status_mobil
+                  WHERE car_id = :car_id ";
+        $this->db->query($query);
+        $this->db->bind('branch_id', $data['branch_id']);
+        $this->db->bind('car_brand_id', $data['car_brand_id']);
+        $this->db->bind('nama_mobil', $data['nama_mobil']);
+        $this->db->bind('jenis_mobil', $data['jenis_mobil']);
+        $this->db->bind('tipe_transmisi', $data['tipe_transmisi']);
+        $this->db->bind('harga_sewa', $data['harga_sewa']);
+        $this->db->bind('foto_mobil', $carImg);
+        $this->db->bind('status_mobil', $data['status_mobil']);
+        $this->db->bind('car_id', $id);
+
+        $this->db->execute();
+
+        return $this->db->affectedRowCount();
+    }
+
+    public function deleteCarById($id)
+    {
+        $this->unlinkCarImg($id);
+        $query = "DELETE FROM {$this->table_name} WHERE car_id = :car_id";
+        $this->db->query($query);
+        $this->db->bind('car_id', $id);
+
+        $this->db->execute();
+
+        return $this->db->affectedRowCount();
+    }
+
+    public function unlinkCarImg($id)
+    {
+        // Mencari Lokasi foto
+        $query = "SELECT foto_mobil FROM {$this->table_name} WHERE car_id = :car_id";
+        $this->db->query($query);
+        $this->db->bind('car_id', $id);
+
+        $row = $this->db->single();
+        $string = 'img/cars/' . $row['foto_mobil'];
+        // Menghapus foto
+        unlink('img/cars/' . $row['foto_mobil']);
+    }
+
+    public function getCarsByKeyword()
+    {
+        $keyword = $_POST['keyword'];
+
+        $query = "UPDATE {$this->table_name} SET 
+         NAMA_MOBIL LIKE :KEYWORD OR  
+         JENIS_MOBIL LIKE :KEYWORD OR
+         TIPE_TRANSMISI LIKE :KEYWORD OR 
+         MERK_MOBIL LIKE :KEYWORD OR 
+         WHERE CAR_ID = :CAR_ID ";
+
+        $this->db->query($query);
+        $this->db->bind('KEYWORD', "%$keyword%");
+        return $this->db->resultSet();
+    }
+}
